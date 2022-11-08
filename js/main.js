dde2ed6f4b21144a23da0f66f3560b2855c951e8
daiana <daianamancuso3@gmail.com>
2022-11-08 16:10:20 -0300

    commit para entrega

--- js/main.js
+++ js/main.js
@@ -5,11 +5,21 @@ let nombre = prompt("ingrese su nombre")
 let apellido = prompt("ingrese su apellido")
 console.log("Bienvenido " + nombre + " " + apellido + " !") 
 
+// creacion de un objeto
+class Producto{
+    constructor (id,nombre,precio){
+        this.id=id;
+        this.nombre=nombre;
+        this.precio=precio;
+    }
+}
+
+const producto1 = new Producto (1,"Lemon Pie",2500);
 
 
 //Definicion de array de objetos con los productos del emprendimiento
 const productos = [
-    {id:1, nombre:"Lemon Pie", precio: 2500}, 
+    producto1,
     {id:2, nombre: "Torta Brownie", precio:3000}, 
     {id:3, nombre: "Crumble de manzana", precio:2900}, 
     {id:4, nombre: "Parfait de Cafe", precio:3100}, 
@@ -20,7 +30,7 @@ console.log(productos)
 
 precio_acumulador= 0;
 
-// funcion para que con id, el usuario pueda ir agregando los productos que quiera
+// Funcion para que usando el id, el usuario pueda ir agregando los productos que quiera
 function cargar_productos (){
     let cargar = "Vaya agregando de a uno los productos que desee comprar. Cuando finalice, escriba: '9' \n\n";
     for (let producto of productos) { 
@@ -38,6 +48,7 @@ function cargar_productos (){
             let producto = buscar_producto(id_producto); 
             console.log(producto); 
             agregar_producto(producto);
+
             precio_acumulador = precio_acumulador + producto.precio
             console.log("subtotal: $" + precio_acumulador)
         } else if (id_producto == 9){
@@ -49,7 +60,7 @@ function cargar_productos (){
     }
 }
 
-// cuando elija el producto que quiere, que el sistema lo busque con find
+// cuando elija el producto que quiere, que el sistema lo busque con funcion de orden superior find
 function buscar_producto (id){ 
     return productos.find((elemento) => elemento.id == id); 
 }
@@ -59,7 +70,7 @@ const carrito = [];
 
 function agregar_producto (producto){ 
     carrito.push(producto); 
-    console.log("producto agregado");
+    console.log("Producto agregado");
     console.log(carrito);
     console.log(carrito.length)
 
@@ -83,7 +94,7 @@ mostrar_carrito();
             descuento_porcentaje = (precio_acumulador*10)/100;
             descuento = precio_acumulador - descuento_porcentaje
 
-            console.log("total a abonar con descuento por cantidad: $" + descuento);
+            console.log("Total a abonar con descuento por cantidad: $" + descuento);
         } else if (cantidad <3){
             console.log("Total a abonar: $" + precio_acumulador)
         }
@@ -107,11 +118,3 @@ mostrar_carrito();
 
     descuento_forma_pago();
 
-// Mensaje de la pagina para aplicar funcion de orden superior (muestra los productos que terminas llevando)
-function mensaje_final(array,funcion){
-    for (const el of array)
-    funcion(el)
-}
-
-console.log("Productos comprados")
-mensaje_final(carrito, console.log)
